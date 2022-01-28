2ce920361c9d05993c53e0aea7673540d1dc9f5e
matthufft <matt@blkmarket.co>
2022-01-28 15:27:10 -0700

    adds caption boxes to memes

--- src/views/SingleMeme.svelte
+++ src/views/SingleMeme.svelte
@@ -1,28 +1,47 @@
 <script>
-    import Meme from '../components/meme.svelte';
     import {currentMeme} from '../stores.js';
-    import {get} from 'svelte/store';
 
     let data = $currentMeme;
+    let boxes = [];
+    console.log('data', data);
+
+    for(let i = 0; i < data.box_count; i++){
+        let box = { id: Math.floor(Math.random() * 9999) }
+        boxes.push(box);
+    }
+    console.log(boxes);
 
 </script>
 
-<h1> {data.name} </h1>
-<div class="meme-card" id={data.id} style="background-image: url({data.url})">
+<div class="container">
+    <h1> {data.name} </h1>
+    <div class="meme-card" id={data.id} style="background-image: url({data.url}); width:{data.width}px; height:{data.height}px">
+        {#each boxes as box (box.id)}
+            <input type="text" class="caption" placeholder="Add your own caption here">
+        {/each}
+    </div>
 </div>
 
 <style> 
-.meme-card {
+.container {
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+}
+.meme-card {
+    position: relative;
+    display: flex;
+    flex-direction: column;
     border: 1px solid black;
     border-radius: 6px;
-    width: 17vw;
-    height: 17vw;
     padding: .5rem;
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
+}
+
+.meme-card .caption {
+    position: absolute;
 }
 
 .meme-card:hover {
